eb6c7eb700b2c6b5edb2b590a69ea830ac74dafc
Lourran <achimid@hotmail.com>
2024-10-21 22:03:04 -0300

    Removendo anotação comentada e esquecida.

--- src/test/java/br/com/achimid/lo_invest_api/LoInvestApiApplicationTests.java
+++ src/test/java/br/com/achimid/lo_invest_api/LoInvestApiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-//@Import(TestcontainersConfiguration.class)
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class LoInvestApiApplicationTests {
 
